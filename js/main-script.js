87fe4fe196e3e49b9d7bbce45b3585f2cf7b9402
mrfeeder <thanh.tran.twentyciasia@gmail.com>
2018-01-22 00:47:25 +0700

    drag drop function

--- js/main-script.js
+++ js/main-script.js
@@ -1,8 +1,9 @@
 (function($){
     $(document).ready(function(){
-        var area_element_for_drag = [$(".tab-content div"),$(".area-for-drop").find('*')];
-        var area_element_for_drop = [$(".area-for-drop"),$(".area-for-drop").find('*')];
-        $(".area-for-drop").sortable();
+        var area_element_for_drag = [$(".tab-content div"),$(".dropindrop")];
+        var area_element_for_drop = [$(".area-for-drop"),$(".dropindrop")];
+        var count = 1;
+        // $(".area-for-drop").sortable();
         $(".area-for-drop").disableSelection();
         $.each(area_element_for_drag,function(){
             $(this).draggable({
@@ -17,9 +18,12 @@
                 hoverClass : 'ui-state-highlight',
                 accept: ":not(.ui-sortable-helper)",
                 drop : function(ev, ui) {
-                    $(ui.draggable).clone().appendTo($(this));
+                    var _draggable = $(ui.draggable);
+                    // _draggable = _draggable.attr("a");
+                    _draggable.clone().appendTo($(this)).attr('id', 'a' + count).addClass( "dropindrop ui-droppable" );
                     $(ui.droppable).clone().appendTo($(this));
                     // $(ui.draggable).remove();
+                    count ++ ;
                 }
             });
         });
