3477c8f1281b933559e780ce8589372d055149fb
mrfeeder <thanh.tran.twentyciasia@gmail.com>
2018-01-28 02:21:28 +0700

    try to fix drag and drop fuction

--- js/main-script.js
+++ js/main-script.js
@@ -1,31 +1,34 @@
 (function($){
     $(document).ready(function(){
-        var area_element_for_drag = [$(".tab-content div"),$(".dropindrop")];
-        var area_element_for_drop = [$(".area-for-drop"),$(".dropindrop")];
-        var count = 1;
-        // $(".area-for-drop").sortable();
-        $(".area-for-drop").disableSelection();
-        $.each(area_element_for_drag,function(){
-            $(this).draggable({
-                containment : "#container",
-                helper : 'clone',
-                revert : 'valid',
-                cancel:false
-            });
+        $(".area-for-drop").sortable();
+        // $(".area-for-drop").disableSelection();
+        // $(".dropindrop").droppable({
+        //     drop: function(event, ui) {
+        //     },
+        //     out: function(event, ui){
+        //         $(this).droppable('option', 'accept', '.drag-item');
+        //     }
+        // });
+        $(".tab-content div").draggable({
+            containment : "#container",
+            helper : 'clone',
+            revert : 'valid',
+            cancel:false
         });
-        $.each(area_element_for_drop,function(){
-            $(this).droppable({
-                hoverClass : 'ui-state-highlight',
-                accept: ":not(.ui-sortable-helper)",
-                drop : function(ev, ui) {
-                    var _draggable = $(ui.draggable);
-                    // _draggable = _draggable.attr("a");
-                    _draggable.clone().appendTo($(this)).attr('id', 'a' + count).addClass( "dropindrop ui-droppable" );
-                    $(ui.droppable).clone().appendTo($(this));
-                    // $(ui.draggable).remove();
-                    count ++ ;
-                }
-            });
+        $(".area-for-drop").droppable({
+            hoverClass : 'ui-state-highlight',
+            accept: ":not(.ui-sortable-helper)",
+            drop : function(ev, ui) {
+                var _draggable = $(ui.draggable);
+                // _draggable = _draggable.attr("a");
+                _draggable.clone().appendTo($(this)).addClass( "dropindrop ui-droppable" );
+                // $(this).droppable('option', 'accept', ui.draggable);
+                $(ui.droppable).clone().appendTo($(this));
+                // $(ui.draggable).remove();
+            }
+        });
+        $(".area-for-drop div").ondrag({
+
         });
     });
 })(jQuery);
