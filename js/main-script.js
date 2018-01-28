7940f19c0ff9a14e1eb444151fca1b8d8dd3cb9d
mrfeeder <thanh.tran.twentyciasia@gmail.com>
2018-01-29 01:26:01 +0700

    I detect for the bug. The bug is why after dropable not use to drag element. because elements inside it maybe layer behind. not in front of drop area.  I try use z index but not a success and I try position have the same. after I change to use the outer box to append content maybe I fixed this bug. but it raises a new bug

--- js/main-script.js
+++ js/main-script.js
@@ -1,6 +1,7 @@
 (function($){
-    $(document).ready(function(){
-        $(".area-for-drop").sortable();
+    $(document)
+    .ready(function(){
+        // $(".area-for-drop").sortable();
         // $(".area-for-drop").disableSelection();
         // $(".dropindrop").droppable({
         //     drop: function(event, ui) {
@@ -21,14 +22,29 @@
             drop : function(ev, ui) {
                 var _draggable = $(ui.draggable);
                 // _draggable = _draggable.attr("a");
-                _draggable.clone().appendTo($(this)).addClass( "dropindrop ui-droppable" );
+                _draggable.clone().appendTo($(".outside-area-drop")).addClass( "dropindrop" ).removeClass("ui-draggable ui-draggable-handle");
                 // $(this).droppable('option', 'accept', ui.draggable);
-                $(ui.droppable).clone().appendTo($(this));
+                $(ui.droppable).clone().appendTo($(".outside-area-drop"));
                 // $(ui.draggable).remove();
             }
         });
-        $(".area-for-drop div").ondrag({
-
+        // $(".area-for-drop div").ondrag($(this).removeClass(""));
+        $(".area-for-drop").click(function(){
+            console.log("2");
         });
-    });
+        $(".outside-area-drop div").click(function(){
+            $(this).removeClass("ui-droppable");
+            console.log("1");
+        });
+        $(".area-for-drop div").draggable({
+            drag: function() {
+                $(this).removeClass("ui-droppable");
+            },
+            containment : "#container",
+            revert : 'valid',
+            cancel:false
+        });
+    })
+    // .change(function(){
+    // });
 })(jQuery);
