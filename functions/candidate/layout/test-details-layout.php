ce372adb4b56f9e266a586bfccaf4629d73eaa37
Developer <account@TWENTYCI.ASIA>
2019-01-18 18:15:01 +0700

    check if not have any test

--- functions/candidate/layout/test-details-layout.php
+++ functions/candidate/layout/test-details-layout.php
@@ -3,11 +3,11 @@
     global $wpdb;
     $testdetails = $wpdb->get_results("
     select name, value from ".$wpdb->prefix."cf7_data_entry where data_id in (SELECT data_id FROM ".$wpdb->prefix."cf7_data_entry where name = 'Email' AND value like '%". $emailcandidate ."%')");
-    $new = $testdetails[9];
-    $new2 = $testdetails[8];
-    array_unshift($testdetails, $new2);
-    array_unshift($testdetails, $new);
     if (isset($testdetails) && $testdetails != null) :
+        $new = $testdetails[9];
+        $new2 = $testdetails[8];
+        array_unshift($testdetails, $new2);
+        array_unshift($testdetails, $new);
         foreach ($testdetails as $testdetail) :
 ?>
     <?php
